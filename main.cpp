e7286ac366b69bdbd24feca1e4ec22b9108c58a9
mazinbersy <mazinbersy@aucegypt.edu>
2024-05-04 15:09:16 +0300

    Removed unnecessary comments

--- main.cpp
+++ main.cpp
@@ -1,34 +1,10 @@
 ﻿#include <iostream>
 #include "Theatre.h"
 #include "Movie.h"
-#include <chrono> // for std::chrono
-#include <thread> // for std::this_thread::sleep_for
+#include <chrono> 
+#include <thread> 
 
 using namespace std;
-
-//int main() {
-//    // Create Movie objects
-//    vector<string> times1 = { "10:00AM", "1:00PM", "4:00PM" };
-//    Movie movie1("Movie 1", times1, 5, 10); // Example parameters for rows and columns
-//
-//    vector<string> times2 = { "11:00AM", "2:00PM", "5:00PM" };
-//    Movie movie2("Movie 2", times2, 6, 12); // Example parameters for rows and columns
-//
-//    // Create a vector of Movie objects
-//    vector<Movie> movies = { movie1, movie2 };
-//
-//    // Create a Theatre object with the vector of Movie objects
-//    Theatre theatre(movies);
-//
-//    // Call the bookSeat function
-//    theatre.bookSeat();
-//
-//
-//    theatre.cancelReservation();
-//
-//    return 0;
-//}
-
 
 
 int main()
